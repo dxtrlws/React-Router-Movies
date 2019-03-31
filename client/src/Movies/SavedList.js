a673b0cd7554f99a6955c82a16e99991f80f6b3a
Dexter Lewis <dexter.lewis@gmail.com>
2019-03-31 12:06:20 -0400

    add navlink tag to home link

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -16,7 +16,7 @@ export default class SavedList extends Component {
           </span>
         ))}
         <div className="home-button">
-          <Link to="/">Home</Link>
+          <NavLink to="/">Home</NavLink>
         </div>
       </div>
     );
